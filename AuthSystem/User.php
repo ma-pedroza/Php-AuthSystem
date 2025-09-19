6c0a2673e393105b0e90e0b1dcc86aa8a6201671
ma-pedroza <matheusgomeszpedroza@gmail.com>
2025-09-18 21:03:06 -0300

    Commit 02
    add funções
    - validateEmail
    - validatePassword
    - emailExist

--- AuthSystem/User.php
+++ AuthSystem/User.php
@@ -3,16 +3,16 @@
 class User
 {
 
-    private array $users;
+    public array $users;
 
 
     public function __construct()
     {
         $this->users = [
-            'id' => 1,
+            ['id' => 1,
             'nome' => 'Usuário Base',
             'email' => 'usuariobase@gmail.com',
-            'senha' => 'SenhaUsuarioBase'
+            'senha' => 'SenhaUsuarioBase'],
         ];
     }
 }
--- AuthSystem/Validation.php
+++ AuthSystem/Validation.php
@@ -1,13 +1,59 @@
 <?php
+include 'User.php';
+
+
+
 class Validation
 {
 
-    public function validadeEmail($email)
+    public function validateEmail($email)
     {
-        if ($email == null or $email = '') {
+        if ($email == null or $email == '') {
             echo ('Email não pode ser vazio.');
+            return false;
         }
+
+        if (!preg_match('/@/', $email)) {
+            echo ('Email inválido.');
+            return false;
+        }
+
+        if (!str_ends_with($email, '.com')) {
+            echo ('Email inválido.');
+            return false;
+        }
+
+        return true;
     }
 
-    public function validatePassword($password) {}
+    public function validatePassword($password) {
+        if (strlen($password) < 8) {
+            echo('A senha deve conter pelo menos 8 caracteres.');
+            return false;
+        }
+        if (!preg_match('/\d/', $password)) {
+            echo("A senha deve conter pelo menos um dígito numérico.");
+            return false;
+        }
+
+        if (!preg_match('/[A-Z]/', $password)) {
+            echo("A senha deve conter pelo menos uma letra maiúscula.");
+            return false;
+       }
+
+       return true;
+    }
+
+    public function emailExist($email) {
+        $userClass = new User();
+
+        foreach ($userClass->users as $user) {
+            if ($user['email'] == $email ) {
+                echo('Email já cadastrado');
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
